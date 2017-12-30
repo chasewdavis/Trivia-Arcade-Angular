9ab83b77a2cae1a4589422552198fa49b7ed7873
Chase Davis <chasewdavis7@gmail.com>
2017-12-29 22:44:27 -0800

    dynamic css classes

--- views/select/select.js
+++ views/select/select.js
@@ -1,8 +1,34 @@
 angular.module('App').controller('selectCtrl', function($scope){
-    console.log('from selectCtrl')
 
-    $scope.random = function(){
-        console.log('scrolling')
-    };
+    $scope.categories = [
+        {'cat':'Sports','sel':false},
+        {'cat':'Geography','sel':false},
+        {'cat':'Computer Science','sel':false},
+        {'cat':'Television','sel':false},
+        {'cat':'General Knowledge','sel':false},
+        {'cat':'Movies','sel':false},
+        {'cat':'History','sel':false}
+    ];
+
+    $scope.highscores = [
+        'Bob Ross - 5312',
+        'Greg Grimes - 2343',
+        'Ricky Bobby - 2341',
+        'Michael H. - 2203'
+    ];
+
+    $scope.difficulty = null;
+
+    $scope.selectCat = function(index){  
+        $scope.categories.forEach(e => {
+            e.sel = false
+        })
+        $scope.categories[index].sel = true;
+    }
+
+    $scope.selectDiff = function(difficulty){
+        $scope.difficulty = difficulty;
+        console.log(difficulty)
+    }
 
 })
